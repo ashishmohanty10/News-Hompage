d323b248cbe764d4b64a8c2c8832dde0d07c82ef
ashishmohanty10 <ashishmohaty10@gmail.com>
2024-02-08 14:39:07 +0530

    done

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Web3 from "/image-web-3-desktop.jpg";
+import Web from "/image-web-3-mobile.jpg";
 import Balancer from "react-wrap-balancer";
 import CTA from "./CTA";
 
@@ -9,11 +10,12 @@ const HeroSection = () => {
       <section className="flex flex-col md:flex-row justify-between items-start gap-5">
         {/* left side */}
         <div className="w-full md:w-[70%]">
-          <img src={Web3} alt="" className="" />
+          <img src={Web3} alt="" className="hidden md:block" />
+          <img src={Web} alt="" className=" md:hidden" />
 
           <div className="md:grid grid-cols-2 justify-between items-start my-5 gap-5">
             <div>
-              <Balancer className="text-5xl font-extrabold text-left tracking-wider mb-4 md:mb-0">
+              <Balancer className="text-6xl font-extrabold text-left tracking-wider mb-4 md:mb-0">
                 The Bright Future of Web 3.0?
               </Balancer>
             </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,24 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
 import Logo from "/logo.svg";
 import { Navlinks } from "./Links";
+import Menu from "/icon-menu.svg";
+import CloseMenu from "/icon-menu-close.svg";
 
 const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
+  const handleMenu = () => {
+    setToggleMenu(!toggleMenu);
+  };
+
   return (
-    <nav className="py-5">
-      <section className="container">
+    <nav className="py-5 relative">
+      <section className="container ">
         <div className="flex justify-between items-center">
           <div>
             <img src={Logo} alt="" />
           </div>
 
-          <ul className="flex justify-between items-center gap-5">
+          <ul className="hidden md:flex justify-between items-center gap-5">
             {Navlinks.map(({ href, name }) => (
               <li key={href}>
                 <a
@@ -25,6 +31,32 @@ const Navbar = () => {
               </li>
             ))}
           </ul>
+
+          {/* Mobile navbar  */}
+
+          <div onClick={handleMenu} className="z-50 md:hidden">
+            {toggleMenu ? <img src={CloseMenu}></img> : <img src={Menu}></img>}
+          </div>
+
+          {toggleMenu ? (
+            <div className="md:hidden absolute top-0 right-0 bg-OffWhite px-5 py-32  h-screen w-[200px] flex items-start">
+              <ul className="flex flex-col gap-5">
+                {Navlinks.map(({ href, name }) => (
+                  <li key={href}>
+                    <a
+                      to={href}
+                      target="_self"
+                      className="text-xl font-base text-VerydarkBlue hover:text-SoftOrange duration-75"
+                    >
+                      {name}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          ) : (
+            ""
+          )}
         </div>
       </section>
     </nav>
